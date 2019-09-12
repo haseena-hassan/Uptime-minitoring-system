b46e215aac6b4c9be90978bad54b6e2231584c7f
haseena-hassan <haseena2199@gmail.com>
2019-09-12 13:43:04 +0530

    created server at localhost

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!")
+/*Description : Primary file for the API
+ *Author : haseena-hassan
+ *Date : 12/09/19
+ */
+
+ //Dependancies
+ var http = require('http');
+
+ //creating server that shuold respond to the request with a string
+var str = "Hello Worldd !\n";
+var server = http.createServer(function(request, response){
+    response.end(str);
+});
+
+//start server and listen to port 2000
+server.listen(2000, function(){
+    console.log('The server is listening to port 2000 now ')
+})
