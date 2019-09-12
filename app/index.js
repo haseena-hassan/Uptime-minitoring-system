d21f3c91d511d120973014f5088ea190bc583ee1
haseena-hassan <haseena2199@gmail.com>
2019-09-12 14:28:25 +0530

    parsed request path

--- app/index.js
+++ app/index.js
@@ -5,11 +5,31 @@
 
  //Dependancies
  var http = require('http');
+ var url = require('url');
+
+/*
+    When we create the server and tell it to listen, when someone hits localhost:3000 the function gets called 
+    each time. req object consist of a whole bunch of info on what the user is asking for, ie it contains url key too,
+    which is used by parse function. true means parse the query string which inturn calls the query string module itself
+
+*/
 
  //creating server that shuold respond to the request with a string
-var str = "Hello Worldd !\n";
 var server = http.createServer(function(request, response){
-    response.end(str);
+
+    //Get the url and parse it
+    var parsedUrl = url.parse(request.url, true)    //parsedUrl is now an obj with whole bunch of keys of parsed metadata of the request url that came in
+
+    //Get the path from the url via the parsedUrl object
+    var path = parsedUrl.pathname;    //it is the untrimmed path that the user requests
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');    //string regex : trimming any extraneous '/' frm path
+    
+    //send the response
+    response.end('Hello Worldd !\n');
+
+    //Log what path the person was asking for
+    console.log('Request received at path : ' + trimmedPath);
+
 });
 
 //start server and listen to port 2000
---
+++ app/results.md
@@ -0,0 +1,6 @@
+### How it looks like on running index.js each time !! (sounds nice ryt ;) ?)
+
+
+#### Parsing the request path
+![](app/img/curl1.png)
+![](app/img/listen1.png)
